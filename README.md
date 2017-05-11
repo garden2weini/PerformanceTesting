ef2d66a4384ac3817f24c759fbe66843b8565f34
dora <moses7625@aliyun.com>
2017-05-11 09:14:19 +0800

    adjust sample game cycle

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 mvn spring-boot:run
 
-127.0.0.1:8888/index (HelloController.java)
+Spring MVC
+-HelloController.java
+-127.0.0.1:8888/index
+Spring RESTFul
+-GameCycleC
 
 /Applications/IntelliJ IDEA CE.app/Contents/plugins/maven/lib/maven3/bin/
--- src/main/java/com/agtech/controller/GameCycleController.java
+++ src/main/java/com/agtech/controller/GameCycleController.java
@@ -11,27 +11,38 @@ import javax.script.ScriptEngineManager;
 @RestController
 @RequestMapping("/game")
 public class GameCycleController {
-    // GC状态：当前阶段，（阶段内）关数，是否中累计奖，王炸数, 这个要进缓存-对应gcid
-    static Integer[] gcState = {0, 0, 0, 0};
+    // GC状态：消除赛, 这个要进缓存-对应gcid
+    static Integer[] gcState = {1, 0, 0, 0};
 
     @ResponseBody
     @RequestMapping(path = "/{gameId}/bet", method = {RequestMethod.GET, RequestMethod.POST})
-    public Long gameBet(@PathVariable Integer gameId) {
+    public String gameBet(@PathVariable Integer gameId) {
+        long stakeID = -1;
         // 找到对应的Game Logic实现并处理bet
-        GameLogicA glA = new GameLogicA();
-        long stakeID = glA.gameLogicBet(gameId, 10000, gcState);
-
+        if(gcState[0]==1) {
+            GameLogicA gl = new GameLogicA();
+            stakeID = gl.gameLogicBet(gameId, 10000, gcState);
+        } else if(gcState[0]==2){
+            GameLogicB gl = new GameLogicB();
+            stakeID = gl.gameLogicBet(gameId, 10000, gcState);
+        } else {
+            return "GC over!";
+        }
         ScriptEngine jse = new ScriptEngineManager().getEngineByName("JavaScript");
-        String gcRule = "%d>%d?%d:%d;";
+        String gcRule = "%d==1?(%d==1?0:(%d==8?2:1)):(%d==1?0:2);";
         String tmp = String.format(gcRule, gcState);
         System.out.println(tmp);
 
         try {
             // 通过规则获得下一关是什么
-            System.out.println(jse.eval(tmp));
+            Object tt = jse.eval(tmp);
+            System.out.println("gcState[0]"+ tt);
+            gcState[0] = Integer.valueOf(tt.toString());
+
         } catch (Exception t) {
+            t.printStackTrace();
         }
-        return stakeID;
+        return stakeID + "+" + gcState[0].toString() + "+" + gcState[1].toString() + "+" + gcState[2].toString() + "+" + gcState[3].toString();
     }
 
     class GameLogicA {
@@ -40,8 +51,22 @@ public class GameCycleController {
             // 处理积分
             // 处理奖池
             // 更新阈值 threadshold
-            threadshold[3] = threadshold[3] + 5;
-            threadshold[1] = threadshold[1] + 6;
+            //threadshold[1] = threadshold[1] + 1;
+            threadshold[2] = threadshold[2] + 1;
+
+            // 返回stake id
+            return System.currentTimeMillis();
+        }
+    }
+
+    class GameLogicB {
+        public long gameLogicBet(int gameid, int betPoint, Integer[] threadshold) {
+            // RNG 解释
+            // 处理积分
+            // 处理奖池
+            // 更新阈值 threadshold
+            threadshold[3] = threadshold[3] + 1;
+
             // 返回stake id
             return System.currentTimeMillis();
         }
